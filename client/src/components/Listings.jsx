091ce5d4da4004d2b7315a062c98cfc6dc073c8b
anuu1 <139375617+anuu1@users.noreply.github.com>
2024-09-22 00:29:07 +0530

    Update Listings.jsx

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -18,8 +18,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `http://localhost:3001/properties?category=${selectedCategory}`
-          : "http://localhost:3001/properties",
+          ? `https://home-rental-backend-uwnl.onrender.com/properties?category=${selectedCategory}`
+          : "https://home-rental-backend-uwnl.onrender.com/properties",
         {
           method: "GET",
         }
